be7c9fcc92943b8e4eb193f31a37a63f76e299a9
Lochie Ferrier <lochie@mit.edu>
2016-10-16 15:48:45 -0400

    Tutorial code added

--- opt.py
+++ opt.py
@@ -1,31 +1,28 @@
-"Very simple problem: minimize x while keeping x greater than 1."
-from gpkit import Variable, Model
-
-# Decision variable
-x = Variable('x')
+from gpkit import Variable, Model, units
 
 # Configuration
 n_rotors = Variable('n_rotors',4,'-')
-payload_mass = Variable('payload_mass',1,'kg')
-structures_mass = Variable()
+payload_mass = Variable('payload_mass',0.1,'kg')
+structures_mass = Variable('structures_mass',0.2,'kg')
 
 # Battery
 battery_mass = Variable('battery_mass','kg')
-battery_energy = Variable('battery_energy','kg')
-battery_specificEnergy = Variable('battery_specificEnergy','kg')
+battery_energy = Variable('battery_energy','J')
+battery_specificEnergy = Variable('battery_specificEnergy',2.98e5,'J/kg')
 
 # ESC
 esc_mass = Variable('esc_mass','kg')
 esc_power = Variable('esc_power','W')
-esc_specificPower = Variable('esc_specificPower','W/kg')
+esc_specificPower = Variable('esc_specificPower',500,'W/kg')
 
 # Motor
 motor_mass = Variable('motor_mass','kg')
 motor_power = Variable('motor_power','W')
-motor_specificPower = Variable('motor_specificPower','W/kg')
+motor_specificPower = Variable('motor_specificPower',3007,'W/kg')
 
 # Propeller
-propeller_d = Variable('propeller_d','m')
+propeller_d = Variable('propeller_d',0.1,'m')
+propeller_efficiency = Variable('propeller_efficiency',0.9,'-')
 
 # Performance variables
 t_hover = Variable('t_hover','s')
@@ -34,23 +31,29 @@ P_total = Variable('P_total','W')
 m_total = Variable('m_total','kg')
 
 # Environment
-rho = Variable('rho','kg/m^3')
-g = Variable('g','m')
+rho = Variable('rho',1.225,'kg/m^3') 
+g = Variable('g',9.8,'m/s/s')
 
 # Constraints
-constraints = [m_total >= n_rotors*(esc_mass+motor_mass) + battery_mass,
+constraints = [battery_energy <= battery_specificEnergy*battery_mass,
+			   esc_power <= esc_specificPower*esc_mass,
+			   motor_power <= motor_specificPower*motor_mass,
 			   esc_power >= motor_power,
-			   t_hover <= battery_energy/P_total]
+			   T_total <= 3.141/2 * rho* units('m^3/kg')* (propeller_efficiency*n_rotors*motor_power**(0.6)*propeller_d**(0.6)*units('(N/(W^0.6 * m^0.6))')),
+			   m_total >= n_rotors*(esc_mass+motor_mass) + battery_mass+payload_mass+structures_mass,
+			   P_total >= n_rotors*esc_power,
+			   T_total >= m_total*g,
+			   t_hover <= battery_energy/P_total,
+			   t_hover >= Variable('t_hoverLimit',120,'s')]
 
 # Objective (to minimize)
-objective = 1/t_hover
+# objective = 1/t_hover
+objective = (1/t_hover)**0.0001 * ((m_total*g)/T_total)**5
 
 # Formulate the Model
 m = Model(objective, constraints)
-
 # Solve the Model
 sol = m.solve(verbosity=1)
 
-# print selected results
-print("Optimal cost:  %s" % sol['cost'])
-print("Optimal x val: %s" % sol(x))
+print '\nMax hover time is ' + str((sol(t_hover)*1/60).magnitude) + ' minutes'
+print 'Thrust to weight ratio is ' + str(sol((T_total/(m_total*g))))
