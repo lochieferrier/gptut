89c09f8e2edb92fa3529cad20d4e5ad527fae2e5
Lochie Ferrier <lochie@mit.edu>
2016-12-28 09:52:55 +1100

    Use ureg instead

--- opt.py
+++ opt.py
@@ -1,4 +1,4 @@
-from gpkit import Variable, Model, units
+from gpkit import Variable, Model, ureg
 from shopping import ShoppingCart
 
 # Configuration
@@ -9,8 +9,8 @@ structures_mass = Variable('structures_mass',0.2,'kg')
 # Battery
 battery_mass = Variable('battery_mass','kg')
 battery_energy = Variable('battery_energy','J')
-battery_shopping = ShoppingCart(goods={battery_energy: [200, 7.53, 533]*units.kJ},
-                                bads={battery_mass: [1.189, 0.119, 0.804]*units.kg})
+battery_shopping = ShoppingCart(goods={battery_energy: [200, 7.53, 533]*ureg.kJ},
+                                bads={battery_mass: [1.189, 0.119, 0.804]*ureg.kg})
 
 # ESC
 esc_mass = Variable('esc_mass','kg')
@@ -20,9 +20,9 @@ esc_specificPower = Variable('esc_specificPower',500,'W/kg')
 # Motor
 motor_mass = Variable('motor_mass','kg')
 motor_power = Variable('motor_power','W')
-motor_shopping = ShoppingCart(goods={motor_power: [49, 194.3, 59.2, 384.1]*units.W},
-                              bads={motor_mass: [16., 127, 13, 162]*units.grams})
-
+motor_shopping = ShoppingCart(goods={motor_power: [49, 194.3, 59.2, 384.1]*ureg.W},
+                              bads={motor_mass: [16., 127, 13, 162]*ureg.grams})
+# print type([49, 194.3, 59.2, 384.1])
 # Propeller
 propeller_d = Variable('propeller_d',0.1,'m')
 propeller_efficiency = Variable('propeller_efficiency',0.9,'-')
@@ -42,7 +42,7 @@ constraints = [battery_shopping,
 			   esc_power <= esc_specificPower*esc_mass,
 			   motor_shopping,
 			   esc_power >= motor_power,
-			   T_total <= 3.141/2 * rho* units('m^3/kg')* (propeller_efficiency*n_rotors*motor_power**(0.6)*propeller_d**(0.6)*units('(N/(W^0.6 * m^0.6))')),
+			   T_total <= 3.141/2 * rho* ureg('m^3/kg')* (propeller_efficiency*n_rotors*motor_power**(0.6)*propeller_d**(0.6)*ureg('(N/(W^0.6 * m^0.6))')),
 			   m_total >= n_rotors*(esc_mass+motor_mass) + battery_mass+payload_mass+structures_mass,
 			   P_total >= n_rotors*esc_power,
 			   T_total >= m_total*g,
@@ -55,6 +55,7 @@ objective = (1/t_hover)**0.0001 * ((m_total*g)/T_total)**5
 
 # Formulate the Model
 m = Model(objective, constraints)
+m.debug()
 # Solve the Model
 sol = m.solve(verbosity=1)
 
